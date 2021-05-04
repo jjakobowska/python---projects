cf0806010ebb63053d2926e4526f0cad55ec0290
jjakobowska <61043020+jjakobowska@users.noreply.github.com>
2021-05-04 12:04:55 +0200

    Update server_socket.py

--- server_socket.py
+++ server_socket.py
@@ -12,30 +12,32 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 
 def handle_client(conn, addr): #individual connection between server and client
-    print("[NEW CONNECTION] " + ADDR + " CONECTED")
+    print("[NEW CONNECTION] " + str(ADDR) + " CONECTED")
 
     connected = True
     while connected:
         msg_length = conn.recv(HEADER).decode(FORMAT)
-        msg_length = int(msg_length)
-        msg = conn.recv(msg_length).decode(FORMAT)
-        if msg == DISCONNECT_MESSAGE:
-            connected = False
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
             
         
-        print(addr + msg )
+            print(addr, msg)
+            conn.send("Msg received".encode(FORMAT))
               
     conn.close()
         
     
 def start(): #handle new collection
     server.listen()
-    print("[LISTENING] Server is listening on " + SERVER)
+    print("[LISTENING] Server is listening on " + str(SERVER))
     while True:
         conn, addr = server.accept() #this line waits for a new connection to a server
         thread = threading.Thread(target=handle_client, args=(conn, addr))
         thread.start()
-        print(f"[ACTIVE CONNECTIONS] " + (threading.activeCount() -1))
+        print(f"[ACTIVE CONNECTIONS] " + str(threading.activeCount() -1))
         
 print("[STARTING] Server is starting...")
 start()
